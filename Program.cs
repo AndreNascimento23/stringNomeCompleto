bfc07a77d9e78a46c69dc90e576de4b5910dc0f6
André Nascimento <andre4.20nascimento@outlook.com>
2023-03-25 14:01:01 -0300

    Atividade.Concluida

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.Write($"Digite seu nome....  ");
+
+string nome;
+nome = Console.ReadLine()!;
+
+
+Console.Write($"Digite seu sobrenome....  ");
+
+string sobrenome;
+sobrenome = Console.ReadLine()!;
+
+string nomeCompleto = (nome) + (sobrenome);
+
+Console.WriteLine();
+Console.Write("Nome Completo: ");
+Console.WriteLine($"{nome} { sobrenome}");
+
+Console.Write("Nome de catálogo: ");
+Console.WriteLine($"{sobrenome}, {nome}");
